cc883979adb7c084bd30a050c6b5d29c6a05d113
HimajaPathi-06 <himajapatel01@gmail.com>
2025-08-22 14:51:15 +0530

    Updated models, added privacy policy and terms pages

--- Website/models.py
+++ Website/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Contact(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField()
-    phone = models.CharField(max_length=15)
+    phone = models.CharField(max_length=15,blank=True, null=True)
     subject = models.CharField(max_length=200)
     message = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
